af5edb07394e5272dd52cec46f1ebddcc559497f
redZeroZero <loicphilippe@hotmail.com>
2020-04-16 23:03:20 +0200

    add property stuff

--- src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
@@ -6,17 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
 //@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
+/* @PropertySources({
         @PropertySource("classpath:datasource.properties"),
         @PropertySource("classpath:jms.properties")
-})
+})*/
 public class PropertyConfig {
 
     @Autowired
@@ -50,10 +47,11 @@ public class PropertyConfig {
         return ds;
     }
 
-    @Bean
+   /* @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer
                 = new PropertySourcesPlaceholderConfigurer();
         return propertySourcesPlaceholderConfigurer;
     }
+    */
 }
